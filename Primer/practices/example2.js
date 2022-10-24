3913043bd39fa881c410bf48182b0a4698b432ca
roobon <roobon@gmail.com>
2022-10-24 17:55:42 +0600

    Added some practice files

---
+++ Primer/practices/example2.js
@@ -0,0 +1,10 @@
+function myFunction(param: number) {
+    if (typeof(param) == "number" || typeof(param) == "string") {
+    let result = param + 100;
+    console.log("My result: " + result);
+    } else {
+    throw ("Expected a number or a string: " + param)
+    }
+   }
+   myFunction(1);
+   //myFunction("London");
--- Primer/src/main.ts
+++ Primer/src/main.ts
@@ -1,12 +1,45 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+// if (environment.production) {
+//   enableProdMode();
+// }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+// function myFunction(param:any) {
+//   if (typeof(param) == "number") {
+//   let result = param + 100;
+//   console.log("My result: " + result);
+//   } else {
+//   throw ("Expected a number: " + param)
+//   }
+//  }
+//  myFunction(1);
+//  myFunction("London");
+
+// function myFunction(param:number | string) {
+//   if (typeof(param) == "number" || typeof(param) == "string") {
+//   let result = param as any + 100;
+//   console.log("My result: " + result);
+//   } else {
+//   throw ("Expected a number or a string: " + param)
+//   }
+//  }
+//  myFunction(1);
+//  myFunction("London");
+
+function myFunction(param: number) {
+  if (typeof(param) == "number" || typeof(param) == "string") {
+  let fixed = param.toFixed(2);
+  console.log("My result: " + fixed);
+  } else {
+  throw ("Expected a number or a string: " + param)
+  }
+ }
+ myFunction(1);
+ //myFunction("London");
