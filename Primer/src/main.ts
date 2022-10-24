24948e8406d8d31e44ede3c71b29caa99514f19b
roobon <roobon@gmail.com>
2022-10-24 18:52:12 +0600

    Some practice files

--- Primer/src/main.ts
+++ Primer/src/main.ts
@@ -33,13 +33,23 @@
 //  myFunction(1);
 //  myFunction("London");
 
-function myFunction(param: number) {
-  if (typeof(param) == "number" || typeof(param) == "string") {
-  let fixed = param.toFixed(2);
-  console.log("My result: " + fixed);
-  } else {
-  throw ("Expected a number or a string: " + param)
-  }
- }
- myFunction(1);
+// function myFunction(param: number | string) {
+//   if (typeof(param) == "number" || typeof(param) == "string") {
+//   let fixed = param.toFixed(2);
+//   console.log("My result: " + fixed);
+//   } else {
+//   throw ("Expected a number or a string: " + param)
+//   }
+//  }
+//  myFunction(1);
  //myFunction("London");
+
+let condition: boolean = true;
+let person: string = "Bob";
+const age: number = 40;
+let place;
+console.log("Place value: " + place + " Type: " + typeof(place));
+place = "London";
+console.log("Place value: " + place + " Type: " + typeof(place));
+place = null;
+console.log("Place value: " + place + " Type: " + typeof(place));
