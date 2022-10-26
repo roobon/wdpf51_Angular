f5e94af9cc235e5497839bd2a18316fabbbaf2dc
Syed Ziaul Habib <roobon@gmail.com>
2022-10-27 02:33:37 +0600

    Added some examples in Primer

---
+++ Primer/src/321.ts
@@ -0,0 +1,11 @@
+function myFunction(param: number | string) {
+    if (typeof(param) == "number") {
+    let numberResult = param.toFixed(2);
+    console.log("My result: " + numberResult);
+    } else {
+    let stringResult = param + 100;
+    console.log("My result: " + stringResult);
+    }
+   }
+   myFunction(1);
+   myFunction("London");
