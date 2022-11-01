325f489b19668cfe836461d174c44f8509fe80e6
roobon <roobon@gmail.com>
2022-11-01 19:05:01 +0600

    Added heroes project

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id:100, 
+    name: 'Hero Alam'
+  }
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Rabbany' },
+  { id: 13, name: 'Prof Amjad' },
+  { id: 14, name: 'Eng Aklima' },
+  { id: 15, name: 'Eng Tokey' },
+  { id: 16, name: 'Dr. Tokey' },
+  { id: 17, name: 'Programmer Zannat' },
+  { id: 18, name: 'Eng Anamul' },
+  { id: 19, name: 'Prof. Fayzullah' },
+  { id: 20, name: 'Prof. Naimur' }
+];
